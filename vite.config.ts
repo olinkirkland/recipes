89c8e7b81f6a1bc72ec4703bed12f778f29d0827
Olin Kirkland <me@olinkirk.land>
2024-09-15 20:10:43 +0200

    Add base url

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  server: { host: false }, // For external IP access
+  base: '/recipes',
+  resolve: {
+    alias: {
+      '@': '/src'
+    }
+  }
+});
